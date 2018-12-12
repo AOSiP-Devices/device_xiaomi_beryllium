d05949aff2bd75927e0f2f46306647bf4f46da4b
Akhil Narang <akhilnarang.1999@gmail.com>
2018-12-12 19:36:10 +0530

    beryllium: switch to an old polaris fingerprint
      * Only sane way to not fail safetynet (?)
    
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- lineage_beryllium.mk
+++ lineage_beryllium.mk
@@ -16,7 +16,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Poco F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:8.1.0/OPM1.171019.011/V9.6.18.0.OEJMIFD:user/release-keys"
+BUILD_FINGERPRINT := "Xiaomi/polaris/polaris:8.0.0/OPR1.170623.032/V9.5.19.0.ODGMIFA:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 8.1.0 OPM1.171019.011 V9.6.18.0.OEJMIFD release-keys" \
