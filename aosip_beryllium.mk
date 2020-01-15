930289558120dede60250c2f699ffd59a9d09d4a
Akhil Narang <akhilnarang.1999@gmail.com>
2020-01-15 23:42:51 +0530

    [DNM]beryllium: switch to walleye oreo-mr1 fingerprint for now
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- aosip_beryllium.mk
+++ aosip_beryllium.mk
@@ -16,7 +16,7 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := POCO F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/V11.0.5.0.PEJMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 9 PKQ1.180729.001 V11.0.5.0.PEJMIXM release-keys" \
