96640efe5f102e8af89fb7247c4f399d2d5f22d5
althafvly <althafvly@gmail.com>
2020-12-30 19:02:26 +0530

    beryllium: Update to build fingerprint from MIUI V12.0.2.0
    [akhil: keep in our coral build fp, just update description]
    
    Change-Id: I242c0a0775bf50f37063a743cf54a6ce625170f8
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- aosip_beryllium.mk
+++ aosip_beryllium.mk
@@ -19,7 +19,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V11.0.6.0.QEJMIXM release-keys" \
+    PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.2.0.QEJMIXM release-keys" \
     PRODUCT_NAME="beryllium"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi-rev1
